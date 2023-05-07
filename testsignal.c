b3b211a6ed27d557ad2b34741596ec23bd340ef6
Robin Gareus <robin@gareus.org>
2023-05-07 05:33:46 +0200

    Add LV2 header location compatibility

--- testsignal.c
+++ testsignal.c
@@ -28,7 +28,11 @@
 #include <string.h>
 #include <time.h>
 
-#include "lv2/lv2plug.in/ns/lv2core/lv2.h"
+#ifdef HAVE_LV2_1_18_6
+#include <lv2/lv2core/lv2.h>
+#else
+#include <lv2/lv2plug.in/ns/lv2core/lv2.h>
+#endif
 
 #define TST_URI "http://gareus.org/oss/lv2/testsignal"
 
